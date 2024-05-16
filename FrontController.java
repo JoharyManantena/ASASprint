fe3ae72b86e7ada256e6a7d6ccd93a8f40abaa61
JoharyManantena <manantenajohary@example.com>
2024-05-16 21:46:44 +0300

    test

--- FrontController.java
+++ FrontController.java
@@ -13,7 +13,7 @@ public class FrontController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println(request.getRequestURL());     
+        out.println(request.getRequestURL());     ASFHASJDFHAKJSDF
        
     }
 
