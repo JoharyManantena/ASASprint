7637ba946707df619a67e8702dc2579b729ea52f
JoharyManantena <166318905+JoharyManantena@users.noreply.github.com>
2024-06-21 05:55:23 +0300

    tapitraSprint7

--- src/prom16/fonction/Reflect.java
+++ src/prom16/fonction/Reflect.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
+// import com.thoughtworks.paranamer.AdaptiveParanamer;
+// import com.thoughtworks.paranamer.Paranamer;
+
 public class Reflect {
     public static String getClassName(Object obj) throws Exception {
         return obj.getClass().getName();
@@ -72,6 +75,68 @@ public class Reflect {
             }
         }
         return valiny;
+    }
+
+    // public static String[] parameterNames(Object obj, String methodName)throws Exception {
+    //     Method[] methods = obj.getClass().getDeclaredMethods();
+    //     String[] valiny = new String[0]; 
+    //     for (Method method : methods) {
+    //         if (method.getName().equals(methodName)) {
+    //             try {
+    //                 Paranamer paranamer = new AdaptiveParanamer();
+    //                 valiny = paranamer.lookupParameterNames(method);
+    //                 if (valiny.length == 0) {
+    //                     throw new Exception("No parameter names found for method: " + methodName);
+    //                 }
+    //             } catch (Exception e) {
+    //                 throw new Exception("Error fetching parameter names: " + e.getMessage()+" avec nom methode = "+methodName + " ou "+ method.getName());
+    //             }
+    //             break;
+    //         }
+    //     }
+    //     return valiny;
+    // }
+    
+    public static <T> T process(T object, Object[] lesValeurs) throws Exception {
+        Class<?> clazz = object.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (int i = 0; i < fields.length && i < lesValeurs.length; i++) {
+            Field field = fields[i];
+            field.setAccessible(true); // Pour accéder à des champs privés si nécessaire
+            Object valeur = lesValeurs[i];
+            if (valeur != null) {
+                field.set(object, valeur); // Définition de la valeur de l'attribut
+            } else {
+                field.set(object, getDefaultValue(field.getType())); // Définir une valeur par défaut si null
+            }
+        }
+        return object;
+    }
+
+    public static Object getDefaultValue(Class<?> type) throws Exception{
+        if (type.isPrimitive()) {
+            if (type == int.class) {
+                return 0;
+            } else if (type == boolean.class) {
+                return false;
+            } else if (type == byte.class) {
+                return (byte) 0;
+            } else if (type == short.class) {
+                return (short) 0;
+            } else if (type == long.class) {
+                return 0L;
+            } else if (type == float.class) {
+                return 0.0f;
+            } else if (type == double.class) {
+                return 0.0;
+            } else if (type == char.class) {
+                return '\u0000';
+            }
+        } else {
+            return null;
+        }
+        throw new Exception("Type non supporté : " + type.getName());
     }
 
     public static Class<?> getClassForName(String typeName) throws Exception {
@@ -163,7 +228,7 @@ public class Reflect {
             return (String) result;
         } else {
             String valiny = "";
-            if (result.getClass().getTypeName().compareTo("prom16.fonction.ModelView") == 0) {
+            if (result.getClass().getTypeName().compareTo("controlleur.fonction.ModelView")==0) {
                 valiny = result.getClass().getTypeName();
             }
             else{
